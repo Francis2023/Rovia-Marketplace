c59729f007f768639731ce54577f5d5dc43e27cd
Bola Inc <fngafua2023@gmail.com>
2021-03-24 14:02:12 -0600

    Add router to the app

--- rovia/src/App.js
+++ rovia/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, useHistory, Switch} from 'react-router-dom';
+import { HomePage } from './components/Home';
+import { LoginPage } from './components/Login';
+import { CartPage } from './components/Cart';
+import { ProductPage } from './components/Product';
+import { CreateProduct } from './components/CreateProduct';
+import { SellerPage } from './components/Seller';
+import { WishlistPage } from './components/Wishlist';
 import './App.css';
 
-function App() {
+export default function App() {
+
+  const history = useHistory();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Route path="/login" component={LoginPage} />
+
+    <Route
+      path="/rovia/cart"
+      exact
+      component={() => <CartPage />}
+    />
   );
 }
 
-export default App;
+
